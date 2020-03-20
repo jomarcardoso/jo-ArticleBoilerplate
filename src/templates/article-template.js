1c0989b0b9a26ef380eb4e079eca2429f12e4f70
jomar.cardoso <jomar.cardoso@cwi.com.br>
2020-03-20 01:06:38 -0300

    screen

--- src/templates/article-template.js
+++ src/templates/article-template.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 import '../css/print.css'
+import '../css/screen.scss'
 import Cover from '../components/cover'
 export default function Template({
   data, // this prop will be injected by the GraphQL query below.
@@ -8,14 +9,9 @@ export default function Template({
   const { markdownRemark } = data // data.markdownRemark holds your post data
   const { frontmatter, html } = markdownRemark
   return (
-    <div className="blog-post-container">
-      <div className="blog-post">
-        <Cover {...frontmatter} />
-        <div
-          className="blog-post-content"
-          dangerouslySetInnerHTML={{ __html: html }}
-        />
-      </div>
+    <div className="print">
+      <Cover {...frontmatter} />
+      <div className="page" dangerouslySetInnerHTML={{ __html: html }} />
     </div>
   )
 }
